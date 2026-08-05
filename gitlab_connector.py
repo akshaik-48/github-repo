157312d5f6f99c5527bee69a290d246de27de294
ashaik-48 <akramammi4@gmail.com>
2026-08-05 04:32:18 -0700

    Update gitlab_connector.py

--- gitlab_connector.py
+++ gitlab_connector.py
@@ -18,7 +18,7 @@ class GitLabClient:
     requests are sent with a ``Bearer`` token header.
     """
 
-     __init__(self) -> None:
+     def __init__(self) -> None:
         """Initialise the client with the base URL and auth headers from settings."""
         self._base = settings.gitlab_api_base_url.rstrip("/")
         self._headers = {
